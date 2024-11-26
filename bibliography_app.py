28fb08ea0495876e9a7b7aa839e1169680b8dae1
Creamypies69 <creamypies1365@gmail.com>
2024-11-26 17:20:25 +0800

    Update bibliography_app.py

--- bibliography_app.py
+++ bibliography_app.py
@@ -10,10 +10,10 @@ st.markdown("""
 # Separator
 st.markdown("<hr style='border: 2px solid #5D3FD3; margin: 0;'>", unsafe_allow_html=True)
 
-# Image with shadow
+# Image with shadow and margin
 image_url = "https://raw.githubusercontent.com/Creamypies69/Bibliography-/9c00f063fcdf27e3dc87b1793304ddacbe4f634c/20241121_151401.jpg"
 st.markdown(f"""
-    <div style='text-align: center;'>
+    <div style='text-align: center; margin: 20px 0;'>
         <img src='{image_url}' style='border-radius: 10px; box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);' width='80%'/>
     </div>
 """, unsafe_allow_html=True)
